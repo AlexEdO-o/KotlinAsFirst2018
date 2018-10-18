5f896ccfb16fd2030d5e058addfec9b182025b4c
AlexEdO-o <sashaEduardova@mail.ru>
2018-10-18 04:24:37 +0300

    Решение задач первого урока

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -1,4 +1,5 @@
-@file:Suppress("UNUSED_PARAMETER")
+@file:Suppress("UNUSED_PARAMETER", "UNREACHABLE_CODE")
+
 package lesson1.task1
 
 import kotlinx.html.InputType
@@ -86,7 +87,7 @@ fun angleInRadian(grad: Int, min: Int, sec: Int): Double = grad * PI / 180 + min
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt(sqr(x2 -x1) + sqr(y2 -y1)
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt(sqr(x2 - x1) + sqr(y2 - y1))
 
 /**
  * Простая
@@ -94,9 +95,8 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt(s
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun trackLength(number: Int): Int {
+fun thirdDigit(number: Int): Int = number / 100 % 10
 
-}
 
 /**
  * Простая
@@ -106,7 +106,9 @@ fun trackLength(number: Int): Int {
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
 fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int {
-    val
+    val depart = hoursDepart * 60 + minutesDepart
+    val arrive = hoursArrive * 60 + minutesArrive
+    return arrive - depart
 }
 
 
@@ -118,11 +120,11 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
 fun accountInThreeYears(initial: Int, percent: Int): Double {
-    val p = percent / 100 + 1
+    val p: Double = percent / 100.0 + 1
     var result: Double = initial * p
     result *= p
-    retirn result * p
-
+    return result * p
+}
 
 
 /**
@@ -137,7 +139,6 @@ fun numberRevert(number: Int): Int {
     result += number / 100
     return result
 }
-
 
 
 

bdd85f022331e96b9c2686fc3ab1bd2e6f277f68
AlexEdO-o <sashaEduardova@mail.ru>
2018-10-26 00:54:41 +0300

    третий урок

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -214,9 +214,7 @@ fun revert(n: Int): Int {
  *
  * Использовать операции со строками в этой задаче запрещается.
  */
-fun isPalindrome(n: Int): Boolean {
-    return n == revert(n)
-}
+fun isPalindrome(n: Int): Boolean = n == revert(n)
 
 /**
  * Средняя
